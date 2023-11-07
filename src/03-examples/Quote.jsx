eaf60baa6ce01c8a62aa9b6278c0b333534bc8fc
Juan Mendoza <juan.mendoza@clt.com.py>
2023-11-07 20:28:16 -0300

    useLayoutEffect example - Added box dimension to Quote

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,15 +1,30 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export const Quote = ({ quote, author }) => {
+  const pRef = useRef();
+  const [boxSize, setBoxSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useLayoutEffect(() => {
+    const { height, width } = pRef.current.getBoundingClientRect();
+    setBoxSize({ height, width });
+  }, []);
+
   return (
-    <blockquote className="blockquote">
-      <p>{quote}</p>
-      <footer className="blockquote-footer">{author}</footer>
-    </blockquote>
+    <>
+      <blockquote className="blockquote text-end">
+        <p ref={pRef}>{quote}</p>
+        <footer className="blockquote-footer">{author}</footer>
+        <code>{JSON.stringify(boxSize)}</code>
+      </blockquote>
+    </>
   );
 };
 
 Quote.propTypes = {
   quote: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired
-}
+  author: PropTypes.string.isRequired,
+};
